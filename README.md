4b80dc443d605b3025c8c68d8e7987ac390102ef
Rafael Ribeiro <rafael.pr10@gmail.com>
2019-03-03 16:39:32 -0300

    Adding methods and event handler

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vuejs-course
+
 Vuejs codes from Vue Mastery Code
--- main.js
+++ main.js
@@ -2,10 +2,37 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
-    inventory: 12,
+    inventory: 0,
     onSale: false,
-    details: ["P", "M", "G"]
+    inStock: false,
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    details: ["80% Cotton", "20% Poliester", "Gender-neutral"],
+    sizes: ["P", "M", "G"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeToCart() {
+      if (this.cart > 0) this.cart -= 1;
+      else return 0;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
 Vue.config.devtools = true;
