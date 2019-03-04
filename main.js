e206e816dd25bab9e8ea4aa1c3419bb011f1cafd
Rafael Ribeiro <rafael.pr10@gmail.com>
2019-03-04 18:16:13 -0300

    Working with computed methods

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 var app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue",
     product: "Socks",
     inventory: 0,
     onSale: false,
-    inStock: false,
-    image: "./assets/vmSocks-green-onWhite.jpg",
+    selectedVariant: 0,
     details: ["80% Cotton", "20% Poliester", "Gender-neutral"],
     sizes: ["P", "M", "G"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: "./assets/vmSocks-green-onWhite.jpg"
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        variantQuantity: 0
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 10
       }
     ],
     cart: 0
@@ -30,8 +32,22 @@ var app = new Vue({
       if (this.cart > 0) this.cart -= 1;
       else return 0;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale() {
+      return this.brand + " " + this.product;
     }
   }
 });
