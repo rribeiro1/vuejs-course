f8b66bec0bd8f86a64db56207627e60d81fb9e31
Rafael Ribeiro <rafael.pr10@gmail.com>
2019-03-08 11:29:11 -0300

    Working with form and form validation

--- main.js
+++ main.js
@@ -1,5 +1,80 @@
 var eventBus = new Vue();
 
+Vue.component("product-review", {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+
+    <p v-if="errors.length">Erros:</p>
+      <b>Please, correct the following error(s):</b>
+      <ul>
+        <li v-for="error in errors"> {{ error }}</li>
+      </ul>
+    <p>
+      <label for="name">Name:</label>
+      <input id="name" v-model="name" placeholder="name">
+    </p>
+
+    <p>
+      <label for="review">Review:</label>
+      <textarea id="review" v-model="review"></textarea>
+    </p>
+
+    <p>
+      <label for="rating">Rating:</label>
+      <select id="rating" v-model.number="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+      </select>
+    </p>
+
+    <p>
+      <label for="recommend">Would you recommend this product?</label>
+      <input type="radio" name="recommendation" v-model="recommend" value="yes"> Yes
+      <input type="radio" name="recommendation" v-model="recommend" value="no"> No
+    </p>
+
+    <p>
+      <input type="submit" value="Submit">
+    </p>
+
+  </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      recommend: null,
+      errors: []
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.name && this.rating && this.review && this.recommend) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+          recommend: this.recommend
+        };
+        this.$emit("review-submitted", productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+        this.recommend = null;
+      } else {
+        if (!this.name) this.errors.push("Name must be filled out");
+        if (!this.review) this.errors.push("Review must be filled out");
+        if (!this.rating) this.errors.push("Rating must be filled out");
+        if (!this.recommend) this.errors.push("Recommend is required");
+      }
+    }
+  }
+});
+
 Vue.component("product-cart", {
   template: `
     <div class="cart">
@@ -92,8 +167,22 @@ Vue.component("product", {
       >
         Add to Cart
       </button>
-
     </div>
+
+    <div>
+      <h2>Reviews</h2>
+      <p v-if="!reviews.length"> There are no review yet for this product. </p>
+      <ul>
+        <li v-for="review in reviews">
+          <p>{{ review.name }}</p>
+          <p>Rating: {{ review.rating }}</p>
+          <p>{{ review.review }}</p>
+          <p>Recommended: {{ review.recommend }}</p>
+        </li>
+      </ul>
+    </div>
+
+    <product-review @review-submitted="addReview"></product-review>
   </div>
   `,
   data() {
@@ -118,7 +207,8 @@ Vue.component("product", {
           variantImage: "./assets/vmSocks-blue-onWhite.jpg",
           variantQuantity: 10
         }
-      ]
+      ],
+      reviews: []
     };
   },
   methods: {
@@ -127,6 +217,9 @@ Vue.component("product", {
     },
     updateProduct(index) {
       this.selectedVariant = index;
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview);
     }
   },
   computed: {
